718e6cf9f3d24f43b3087acf499176dfea144aa8
Giedrius Krisciukaitis <giedriusk@gmail.com>
2018-04-10 06:03:34 -0400

    Found malloc meaning.

--- c/strings.c
+++ c/strings.c
@@ -2,25 +2,35 @@
 #include <string.h>
 #include <stdlib.h>
 
-int cmpstr(const void* a, const void* b) {
-	//const char* aa = (const char*)a;
-	//const char* bb = (const char*)b;
-	return 0;//strcmp(aa, bb);
-}
-
 char* longest(char* s1, char* s2) {
-	char* rez = s1;
-	int sz;
-	sz = strlen(rez);
+	int sz_s1 = strlen(s1);
+	int sz_s2 = strlen(s2);
+	int tsz = sz_s1 + sz_s2;
+
+	char *rez = malloc(tsz * sizeof(char));
+	strcpy(rez, s1);
+
+	char temp;
+	int sz = strlen(rez);
+	int i, j;
 	printf("Size: %d\n", sz);
-	qsort(rez, sz, sizeof(char*), cmpstr);
+
+	for (i = 0; i < sz-1; i++) {
+		for (j = 0; j < sz; j++) {
+			if (rez[i] > rez[j]) {
+				temp = rez[i];
+				rez[i] = rez[j];
+				rez[j] = temp;
+			}
+		}
+	}
+
 	return rez;
 }
 
 int main() {
 
 	printf("Start..\n");
-
 	printf("Result: %s\n", longest("aretheyhere", "yestheyarehere"));
 
 	return 0;
