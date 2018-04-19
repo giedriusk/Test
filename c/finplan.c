3a58db7b77661163100a27022b0a82480f76be70
Giedrius Krisciukaitis <giedriusk@gmail.com>
2018-04-19 22:13:14 +0300

    Started Financial Plan Kata.

---
+++ c/finplan.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+unsigned long long finance(unsigned long long n) {
+  unsigned long long rez = n;
+  return rez;
+}
+
+int main() {
+  printf("Result: %llu\n", finance(5));
+  return 0;
+}
