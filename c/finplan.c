e901be35102b78fd20dd53e532ef7786fd875c81
Giedrius Krisciukaitis <giedriusk@gmail.com>
2018-04-23 05:52:54 -0400

    Recursive function added to finplan kata.

--- c/finplan.c
+++ c/finplan.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 
+unsigned long long calc_week(unsigned long long n, unsigned long long rez) {
+
+  int i;
+  for (i = 0; i <= n; i++) rez += i;
+  printf("N: %llu Rez: %llu\n", n, rez);
+
+  if (n > 0) rez += calc_week(n-1, rez);
+  else rez += 0;
+
+  return rez;
+}
+
 unsigned long long finance(unsigned long long n) {
-  unsigned long long rez = n;
+  unsigned long long rez = 0;
+
+  // only positive numbers
+  if (n < 0) return 0;
+
+  // calculate weeks recursively
+  rez = calc_week(n, rez);
+
   return rez;
 }
 
 int main() {
-  printf("Result: %llu\n", finance(5));
+  printf("Result: %llu\n", finance(6));
   return 0;
 }
