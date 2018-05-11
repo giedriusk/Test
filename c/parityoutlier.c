a4be839d963f0c66574c65859f8ebffbaf2097d0
Giedrius Krisciukaitis <giedriusk@gmail.com>
2018-05-12 00:31:44 +0300

    Finished the Parity Outlier kata.

--- c/parityoutlier.c
+++ c/parityoutlier.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
 
 int find_outlier(const int *values, size_t count) {
-  int result = 0;
   int i;
 
-  if (count < 3) return result; // check for the minimal size
+  if (count < 3) return 0; // check for the minimal size
 
-  // calculate the sum of each neibourgh and check if it is even
+  // calculate the sum of two consequent members and check if it is even
   // even + even = even    odd + odd = even
   // if both sums is not even, this means we found an outlier
-  for (i = 1; i < count-1; i++) {
-    if ( (values[i] + values[i+1]) % 2 != 0 && 
-         (values[i] + values[i-1]) % 2 != 0) {
-        return values[i];
+  for (i = 0; i < count; i++) {
+    if (i < count-2) { // all until the last two items
+      if ( (values[i] + values[i+1]) % 2 != 0 && 
+           (values[i] + values[i+2]) % 2 != 0 &&
+           (values[i+1] + values[i+2]) % 2 == 0) {
+          return values[i];
+      }
+    } else { // the last two
+      if ( (values[i] + values[i-1]) % 2 != 0 && 
+           (values[i] + values[i-2]) % 2 != 0 &&
+           (values[i-1] + values[i-2]) % 2 == 0) {
+          return values[i];
+      }
     }
   }
 
-  return result;
+  return 0;
 }
 
 int main() {
-  int values[] = {3, 2, 6, 8, 10};
+  int values[] = {4, 2, 6, 8, 10, 5};
   //int values[] = {1, 3, 5, 7, 8};
   size_t count = sizeof(values)/sizeof(values[0]);
   printf("Result: %d\n", find_outlier(values, count));
--- c/primefactor.c
+++ c/primefactor.c
@@ -4,6 +4,9 @@
 char* factors(int lst) {
   char* rez = malloc(100 * sizeof(char));
   int primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47};
+
+  
+
   rez = "1234";
   return rez;
 }
