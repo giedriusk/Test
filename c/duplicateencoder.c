a9b59f1dbd6f3a2a44d5cb528922023ca0afc012
Giedrius Krisciukaitis <giedriusk@gmail.com>
2018-05-14 06:04:11 -0400

    Started duplicate encoder kata.

---
+++ c/duplicateencoder.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+char *DuplicateEncoder(char *str) {
+  char *result = str;
+  return result;
+}
+
+int main() {
+  printf("Result: %s\n", DuplicateEncoder("abccdefee"));
+  return 0;
+}
