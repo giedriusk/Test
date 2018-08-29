9970c7878c4554847fe8e57c4f4619272451b912
Giedrius Krisciukaitis <giedriusk@gmail.com>
2018-08-29 06:27:18 -0400

    Xes Ohs kata completed.

---
+++ c/xocheck.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool xo(const char* str) {
+
+  char* t;
+  strcpy(t, str);
+  int rez = 0;
+  for (int i = 0; t[i] != '\0'; i++) {
+    if (t[i] == 'o' || t[i] == 'O') rez++;
+    if (t[i] == 'x' || t[i] == 'X') rez--;
+  }
+
+  if (rez == 0) return true;
+  else return false;
+}
+
+int main() {
+  printf("Starting check for x`es and o`s...\n");
+  if (true == xo("xooxx")) printf("TRUE\n");
+  else printf("FALSE\n");
+  return 0;
+}
