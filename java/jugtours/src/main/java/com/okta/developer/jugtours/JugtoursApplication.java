46e1e0a138c513890593a22c105ecf0fdd11553f
Giedrius Krisciukaitis <giedriusk@gmail.com>
2020-04-13 19:34:44 +0300

    SpringBoot + ReactJS app kata.

---
+++ java/jugtours/src/main/java/com/okta/developer/jugtours/JugtoursApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.okta.developer.jugtours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JugtoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JugtoursApplication.class, args);
+	}
+
+}
